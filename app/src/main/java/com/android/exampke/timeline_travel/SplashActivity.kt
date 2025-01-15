5e8088e019a48a8516a22610aff6674c616535bb
Johen In <injunhyun11@gmail.com>
2025-01-15 20:26:26 +0900

    Splash Screen 기본 틀 build

--- app/src/main/java/com/android/exampke/timeline_travel/SplashActivity.kt
+++ app/src/main/java/com/android/exampke/timeline_travel/SplashActivity.kt
@@ -8,40 +8,67 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.android.exampke.timeline_travel.ui.theme.Timeline_travelTheme
 
 class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent{
+        setContent {
             Timeline_travelTheme {
                 SplashScreen()
             }
         }
         Handler(Looper.getMainLooper()).postDelayed({
-
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-
             finish()
-        },2000)
+        }, 2500)
 
     }
 }
 
+@Preview
+
+
 @Composable
 fun SplashScreen() {
-    Box(modifier = Modifier
-        .paint(painterResource(R.drawable.splashbackgroundimage),
-            contentScale = ContentScale.FillHeight)
-        .fillMaxSize()
-){
+    Column(
+        modifier = Modifier
+            .paint(
+                painterResource(R.drawable.splashbackgroundimage),
+                contentScale = ContentScale.FillHeight
+            )
+            .fillMaxSize()
+            .padding(vertical = 100.dp, horizontal = 30.dp)
+    ) {
+        Text(
+            "[앱 이름]",
+            color = Color.Black,
+            fontSize = 80.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.ExtraBold,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Text("AI", color = Color.White, fontSize = 40.sp)
+        Text("한국 가이드", color = Color.White, fontSize = 40.sp)
     }
 }
