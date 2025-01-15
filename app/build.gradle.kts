46e4f692332e9b6a635f8085f510a00bcedaacf4
Johen In <injunhyun11@gmail.com>
2025-01-15 16:34:01 +0900

    즐겨찾기 작업 중

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    //room
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -56,4 +59,16 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //room
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+
+    // If this project uses any Kotlin source, use Kotlin Symbol Processing (KSP)
+    // See Add the KSP plugin to your project
+    ksp("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
 }
---
+++ app/src/main/java/com/android/exampke/timeline_travel/SaveDataDao.kt
@@ -0,0 +1,19 @@
+package com.android.exampke.timeline_travel
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SaveDataDao {
+    @Query("SELECT * FROM savedata")
+    fun getAll(): Flow<List<SaveData>>
+
+    @Insert
+    fun insertAll(vararg saveDatas: SaveData)
+
+    @Delete
+    fun delete(saveData: SaveData)
+}
