05585b8fe565671b8d736d72c1248adff304cb98
Johen In <injunhyun11@gmail.com>
2025-01-16 11:31:48 +0900

    MainActivity UI 구성 더 깔끔하게

--- app/src/main/java/com/android/exampke/timeline_travel/MainActivity.kt
+++ app/src/main/java/com/android/exampke/timeline_travel/MainActivity.kt
@@ -8,13 +8,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -25,8 +28,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.android.exampke.timeline_travel.ui.theme.Timeline_travelTheme
 import com.android.exampke.timeline_travel.viewmodel.MapViewModel
 import com.android.exampke.timeline_travel.viewmodel.ShowGoogleMap
@@ -54,13 +61,14 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
 fun MainScreen(
     modifier: Modifier
 ) {
-    Column(modifier = modifier
-        .verticalScroll(rememberScrollState())) {
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+    ) {
         Row(
             horizontalArrangement = Arrangement.SpaceEvenly,
             modifier = Modifier
@@ -83,76 +91,70 @@ fun MainScreen(
             }
 
         }
-        Text("요즘 뜨는 장소")
+        Text(
+            "요즘 뜨는 장소",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.ExtraBold,
+            lineHeight = 50.sp,
+            modifier = Modifier.padding(start = 15.dp)
+        )
         Row(
             modifier = Modifier
                 .padding(end = 8.dp)
                 .horizontalScroll(rememberScrollState())
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.splashbackgroundimage),
-                contentDescription = "oui",
-                modifier = Modifier
-                    .height(350.dp)
-                    .padding(start = 8.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .clickable {},
-            )
-            Image(
-                painter = painterResource(id = R.drawable.splashbackgroundimage),
-                contentDescription = "oui",
-                modifier = Modifier
-                    .height(350.dp)
-                    .padding(start = 8.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .clickable {},
-            )
-            Image(
-                painter = painterResource(id = R.drawable.splashbackgroundimage),
-                contentDescription = "oui",
-                modifier = Modifier
-                    .height(350.dp)
-                    .padding(start = 8.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .clickable {},
-            )
-            Image(
-                painter = painterResource(id = R.drawable.splashbackgroundimage),
-                contentDescription = "oui",
-                modifier = Modifier
-                    .height(350.dp)
-                    .padding(start = 8.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .clickable {},
-            )
-            Image(
-                painter = painterResource(id = R.drawable.splashbackgroundimage),
-                contentDescription = "oui",
-                modifier = Modifier
-                    .height(350.dp)
-                    .padding(start = 8.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .clickable {},
-            )
-            Image(
-                painter = painterResource(id = R.drawable.splashbackgroundimage),
-                contentDescription = "oui",
-                modifier = Modifier
-                    .height(350.dp)
-                    .padding(start = 8.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .clickable {},
-            )
+            TrendLandmark()
+            TrendLandmark()
+            TrendLandmark()
+            TrendLandmark()
+            TrendLandmark()
+            TrendLandmark()
 
         }
-        Text("근처 랜드마크")
+        Spacer(modifier = Modifier.height(50.dp))
+        Text(
+            "근처 랜드마크",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.ExtraBold,
+            lineHeight = 50.sp,
+            modifier = Modifier.padding(start = 15.dp)
+        )
         ShowGoogleMap(
             mapViewModel = MapViewModel(),
             modifier = Modifier
                 .fillMaxWidth()
                 .height(300.dp)
         )
-        Spacer(modifier = Modifier
-            .height(30.dp))
+        Spacer(
+            modifier = Modifier
+                .height(30.dp)
+        )
+    }
+}
+
+@Composable
+private fun TrendLandmark() {
+    Column(
+        modifier = Modifier
+            .padding(start = 8.dp)
+            .width(intrinsicSize = IntrinsicSize.Max)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.splashbackgroundimage),
+            contentDescription = "oui",
+            modifier = Modifier
+                .height(350.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .clickable {},
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 5.dp)
+        ) {
+            Text("경복궁", lineHeight = 30.sp, fontSize = 20.sp, fontWeight = FontWeight.Bold)
+        }
+        Text("서울시 종로구", lineHeight = 14.sp, modifier = Modifier.padding(start = 5.dp))
     }
 }
